21e73ff7ffee562cfd051bab4886248fee5d069c
Jaxe <42095078+Jaxe-Dev@users.noreply.github.com>
2020-08-22 15:57:22 +0800

    v1.3
    - Added support for RimWorld 1.2
    - Removed support for RimWorld 1.0

--- Source/Mod.cs
+++ Source/Mod.cs
@@ -8,7 +8,7 @@ namespace PublisherPlus
     {
         public const string Id = "PublisherPlus";
         public const string Name = Id;
-        public const string Version = "1.1";
+        public const string Version = "1.3";
 
         public static bool ExperimentalMode { get; set; }
 
